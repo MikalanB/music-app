2cbfc58d8fc430f90e05cabeb5c0d447ba51921b
MikalanB <77364785+MikalanB@users.noreply.github.com>
2022-07-22 22:42:46 -0400

    testing

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Mikalan Ballentine" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          IS this working? <code>pages/index.js</code>
         </p>
       </main>
 
